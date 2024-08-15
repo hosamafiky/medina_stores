d60e159dd6936e204587225450abb2d685a83187
HUSSAM <hosamafiky@gmail.com>
2024-08-15 15:02:51 +0300

    refactor: Update test case times in fastlane report.xml
    This commit updates the test case times in the fastlane report.xml file. The times for the "flutter clean" and "flutter build apk --release --no-tree-shake-icons" test cases have increased, while the time for the "firebase_app_distribution" test case has decreased. These changes reflect the updated performance of the test cases.
    
    These changes improve the accuracy of the test case times in the fastlane report.

--- lib/features/slider/presentation/widgets/dots_indicator_widget.dart
+++ lib/features/slider/presentation/widgets/dots_indicator_widget.dart
@@ -10,23 +10,37 @@ class SliderDotsIndicator extends StatelessWidget {
     final palette = context.colorPalette;
     return BlocBuilder<SliderCubit, SliderState>(
       builder: (context, state) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(
-            state.sliders.length,
-            (index) {
-              return AnimatedContainer(
-                duration: const Duration(milliseconds: 200),
-                width: state.sliderIndex == index ? 32.w : 24.w,
-                height: 4.h,
-                decoration: BoxDecoration(
-                  color: state.sliderIndex == index ? palette.currentSliderDotColor : palette.otherSliderDotColor,
-                  borderRadius: BorderRadius.circular(50),
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 0.7.sw,
+          ),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              final maxWidth = constraints.maxWidth;
+              final width = maxWidth / state.sliders.length;
+              final spacing = 0.01.sw;
+              final dotWidth = width - spacing;
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  state.sliders.length,
+                  (index) {
+                    return AnimatedContainer(
+                      duration: const Duration(milliseconds: 200),
+                      width: state.sliderIndex == index ? dotWidth : dotWidth * 0.8,
+                      height: 4.h,
+                      decoration: BoxDecoration(
+                        color: state.sliderIndex == index ? palette.currentSliderDotColor : palette.otherSliderDotColor,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    );
+                  },
                 ),
-              );
+              ).withSpacing(spacing: spacing);
             },
           ),
-        ).withSpacing(spacing: 6.w);
+        );
       },
     );
   }
