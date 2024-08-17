ddebb7ba525850ad5de89163abdee230dede33aa
Hussam <hosamafiky@gmail.com>
2024-08-17 13:59:52 +0300

    refactor: Update SliderRemoteDataSourceImpl to remove hardcoded slider model

--- lib/features/slider/data/datasources/slider_remote_data_source.dart
+++ lib/features/slider/data/datasources/slider_remote_data_source.dart
@@ -16,20 +16,7 @@ class SliderRemoteDataSourceImpl implements SliderRemoteDataSource {
           request,
           mapper: (json) => ApiResponse.fromMapSuccess(
             json,
-            mapper: (data) => List<SliderModel>.from(data['data'].map((x) => SliderModel.fromMap(x)))
-              ..insert(
-                0,
-                SliderModel(
-                  id: data['data'].length + 1,
-                  title: "Demo Slider",
-                  description: "Description of the slider",
-                  url: "Url",
-                  type: SliderMediaType.video,
-                  order: 0,
-                  media: "https://videos.pexels.com/video-files/8471681/8471681-uhd_2732_1440_25fps.mp4",
-                  createdAt: DateTime.now(),
-                ),
-              ),
+            mapper: (data) => List<SliderModel>.from(data['data'].map((x) => SliderModel.fromMap(x))),
           ),
         );
   }
