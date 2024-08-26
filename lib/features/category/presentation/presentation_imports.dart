b9d53e4e9f5e399b0d578d1f170bc3c9aa287579
Hussam <hosamafiky@gmail.com>
2024-08-26 14:15:37 +0300

    refactor: Generate localization files for easy localization package

--- lib/features/category/presentation/presentation_imports.dart
+++ lib/features/category/presentation/presentation_imports.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medina_stores/config/resources/locale_keys.g.dart';
 import 'package:medina_stores/core/shared_widgets/core_widgets/shimmer_widget.dart';
 
 import '../../../core/error/failures.dart';
