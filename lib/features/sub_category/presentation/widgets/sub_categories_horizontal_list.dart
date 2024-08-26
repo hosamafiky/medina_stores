d67e527837ea9a124a6ac2c73f3772afcbc01e3b
Hussam <hosamafiky@gmail.com>
2024-08-26 13:34:46 +0300

    Handled Loading, error, empty and success states

--- lib/features/sub_category/presentation/widgets/sub_categories_horizontal_list.dart
+++ lib/features/sub_category/presentation/widgets/sub_categories_horizontal_list.dart
@@ -5,22 +5,50 @@ class SubCategoriesHorizontalList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocSelector<SubCategoryCubit, SubCategoryState, ({UsecaseStatus status, Failure? failure, List<SubCategory> subCategories})>(
-      selector: (state) => (status: state.subCategoriesStatus, failure: state.subCategoriesFailure, subCategories: state.subCategories),
-      builder: (context, state) {
-        return SizedBox(
-          height: 150.h,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-            itemCount: state.subCategories.length,
-            separatorBuilder: (context, index) => SizedBox(width: 16.w),
-            itemBuilder: (context, index) {
-              final subCategory = state.subCategories[index];
-              return SubCategoryCard(subCategory);
-            },
-          ),
-        );
+    return SizedBox(
+      height: 150.h,
+      child: BlocSelector<SubCategoryCubit, SubCategoryState, ({UsecaseStatus status, Failure? failure, List<SubCategory> subCategories})>(
+        selector: (state) => (status: state.subCategoriesStatus, failure: state.subCategoriesFailure, subCategories: state.subCategories),
+        builder: (context, state) {
+          final isLoading = state.status == UsecaseStatus.running;
+          if (isLoading) {
+            return const _SubCategoriesList.skeleton();
+          }
+          if (state.failure != null) {
+            return Center(child: Text("Failed to load sub categories ${state.failure!.response.message}"));
+          }
+          if (state.subCategories.isEmpty) {
+            return const Center(child: Text("No sub categories found"));
+          }
+
+          return _SubCategoriesList(state.subCategories);
+        },
+      ),
+    );
+  }
+}
+
+class _SubCategoriesList extends StatelessWidget {
+  const _SubCategoriesList(this.subCategories) : _isSkeleton = false;
+
+  const _SubCategoriesList.skeleton()
+      : _isSkeleton = true,
+        subCategories = const [];
+
+  final List<SubCategory> subCategories;
+  final bool _isSkeleton;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+      itemCount: _isSkeleton ? 5 : subCategories.length,
+      separatorBuilder: (context, index) => SizedBox(width: 16.w),
+      itemBuilder: (context, index) {
+        if (_isSkeleton) return SubCategoryCard.skeleton();
+        final subCategory = subCategories[index];
+        return SubCategoryCard(subCategory);
       },
     );
   }
